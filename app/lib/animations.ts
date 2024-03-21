fcc55df87a6fb984ad446f6002091578ee5181b8
Petr Grabovský <petrgrabovsky@gmail.com>
2024-03-21 19:22:16 +0100

    Přidány nové stavy pro aplikaci.

--- app/lib/animations.ts
+++ app/lib/animations.ts
@@ -4,11 +4,11 @@ export const drawOutlineLogoPrimaryPath: Variants = {
     initial: {
         pathLength: 0, 
         fill: "#00000000", 
-        stroke: "#F6F6F6"
+        stroke: "var(--color-third)"
     },
     animate: {
         pathLength: 1, 
-        fill: "#F6F6F6", 
+        fill: "var(--color-third)", 
         stroke: "#00000000",
         transition: {
             pathLength: { delay: 1.2, type: "spring", duration: 2, bounce: 0 }, 
@@ -22,11 +22,11 @@ export const drawOutlineLogoSecondaryPath: Variants = {
     initial: {
         pathLength: 0, 
         fill: "#00000000", 
-        stroke: "#84BF61"
+        stroke: "var(--color-sixth)"
     },
     animate: {
         pathLength: 1, 
-        fill: "#84BF61", 
+        fill: "var(--color-sixth)", 
         stroke: "#00000000",
         transition: { 
             pathLength: { delay: 1.2, type: "spring", duration: 2, bounce: 0 }, 
--- app/lib/globalContext.tsx
+++ app/lib/globalContext.tsx
@@ -1,13 +1,21 @@
-import { Dispatch, ReactNode, SetStateAction, createContext, useContext, useState } from "react"
+import { 
+    Dispatch, ReactNode, SetStateAction, createContext, 
+    useContext, useState, useEffect 
+} from "react"
+import throttle from "lodash/throttle"
 
 interface GlobalContextType {
     isMenuOpen: boolean
     setIsMenuOpen: Dispatch<SetStateAction<boolean>>
+    isScreenSmall: boolean
+    isScrollTop: boolean
 }
 
 const GlobalContext = createContext<GlobalContextType>({
     isMenuOpen: false,
-    setIsMenuOpen: () => {}
+    setIsMenuOpen: () => {},
+    isScreenSmall: true,
+    isScrollTop: true
 })
 
 export const useGlobalContext = (): GlobalContextType => {
@@ -21,10 +29,40 @@ interface GlobalContextProviderProps {
 
 export const GlobalContextProvider: React.FC<GlobalContextProviderProps> = ({ children }) => {
     const [ isMenuOpen, setIsMenuOpen ] = useState<boolean>( false )
+    const [ isScreenSmall, setIsScreenSmall ] = useState<boolean>( true )
+    const [ isScrollTop, setIsScrollTop ] = useState<boolean>( true )
+
+    const handleScreenResize = throttle(() => {
+        const screenIsSmall = window.innerWidth < 640
+        setIsScreenSmall( screenIsSmall )
+        if ( !screenIsSmall ) setIsMenuOpen( false )   
+    }, 100)
+
+    const handleScroll = throttle(() => {
+        const scrollIsTop = window.scrollY < 50
+        setIsScrollTop( scrollIsTop )
+    }, 100)
+
+    useEffect(() => {
+        if ( typeof window === "undefined" ) return
+      
+        window.addEventListener( "resize", handleScreenResize )
+        window.addEventListener( "scroll", handleScroll )
+      
+        handleScreenResize()
+        handleScroll()
+      
+        return () => {
+          window.removeEventListener( "resize", handleScreenResize )
+          window.removeEventListener( "scroll", handleScroll )
+          handleScreenResize.cancel()
+          handleScroll.cancel()
+        }
+      }, [])
 
     return (
         <GlobalContext.Provider
-            value={{ isMenuOpen, setIsMenuOpen }}
+            value={{ isMenuOpen, setIsMenuOpen, isScreenSmall, isScrollTop }}
         >
             { children }
         </GlobalContext.Provider>
